5dbf0627b3cd8f87760f2c07ceda3191e0fcf376
David Royer <droyer01@gmail.com>
2020-01-12 08:16:04 -0500

    chore: update

--- static/admin/cms.js
+++ static/admin/cms.js
@@ -5,44 +5,44 @@ const { CMS } = window
 CMS.registerPreviewStyle('example.css')
 // CMS.registerPreviewStyle(styles.toString(), { raw: true })
 
-const TeamsPreview = createClass({
-  // For list fields, the widgetFor function returns an array of objects
-  // that you can map over in your template. If our field is a list of
-  // authors containing two entries, with fields `name` and `description`,
-  // the return value of `widgetsFor` would look like this:
-  //
-  // [{
-  //   data: { name: 'Mathias', description: 'Co-Founder'},
-  //   widgets: { name: (<WidgetComponent>), description: (WidgetComponent>)}
-  // },
-  // {
-  //   data: { name: 'Chris', description: 'Co-Founder'},
-  //   widgets: { name: (<WidgetComponent>), description: (WidgetComponent>)}
-  // }]
-  //
-  // Templating would look something like this:
+// const TeamsPreview = createClass({
+//   // For list fields, the widgetFor function returns an array of objects
+//   // that you can map over in your template. If our field is a list of
+//   // authors containing two entries, with fields `name` and `description`,
+//   // the return value of `widgetsFor` would look like this:
+//   //
+//   // [{
+//   //   data: { name: 'Mathias', description: 'Co-Founder'},
+//   //   widgets: { name: (<WidgetComponent>), description: (WidgetComponent>)}
+//   // },
+//   // {
+//   //   data: { name: 'Chris', description: 'Co-Founder'},
+//   //   widgets: { name: (<WidgetComponent>), description: (WidgetComponent>)}
+//   // }]
+//   //
+//   // Templating would look something like this:
 
-  render() {
-    return h(
-      'div',
-      {},
+//   render() {
+//     return h(
+//       'div',
+//       {},
 
-      // This is a static header that would only be rendered once for the entire list
-      h('h1', {}, 'Teams'),
+//       // This is a static header that would only be rendered once for the entire list
+//       h('h1', {}, 'Teams'),
 
-      // Here we provide a simple mapping function that will be applied to each
-      // object in the array of authors
-      this.props.widgetsFor('team').map(function(team, index) {
-        return h(
-          'section',
-          { key: index },
-          h('hr', {}),
-          h('h2', {}, team.getIn(['data', 'name'])),
-          team.getIn(['widgets', 'position'])
-        )
-      })
-    )
-  }
-})
+//       // Here we provide a simple mapping function that will be applied to each
+//       // object in the array of authors
+//       this.props.widgetsFor('team').map(function(team, index) {
+//         return h(
+//           'section',
+//           { key: index },
+//           h('hr', {}),
+//           h('h2', {}, team.getIn(['data', 'name'])),
+//           team.getIn(['widgets', 'position'])
+//         )
+//       })
+//     )
+//   }
+// })
 
-CMS.registerPreviewTemplate('teams', TeamsPreview)
+// CMS.registerPreviewTemplate('teams', TeamsPreview)
