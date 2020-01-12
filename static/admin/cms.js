1b40a17856094edd79e7c01cee477e7facdf492e
David Royer <droyer01@gmail.com>
2020-01-11 20:34:31 -0500

    chore: update

---
+++ static/admin/cms.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-webpack-loader-syntax
+// import styles from '!css-loader!sass-loader!../main.scss'
+const { CMS } = window
+
+CMS.registerPreviewStyle('example.css')
+// CMS.registerPreviewStyle(styles.toString(), { raw: true })
